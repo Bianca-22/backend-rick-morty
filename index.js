3814f6a8fd37e3c049d815b20675e6339d5aac7d
Bianca Monteiro Corrêa <biancamonteiro22.06@gmail.com>
2021-09-03 17:57:19 -0300

    conexão com mongodb atlas

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ require("dotenv").config();
 const ObjectId = mongodb.ObjectId;
 
 (async () => {
-    const dbHost = process.env.DB_HOST;
-    const dbPort = process.env.DB_PORT;
+    const dbUser = process.env.DB_USER;
+    const dbPassword = process.env.DB_PASSWORD;
     const dbName = process.env.DB_NAME;
+	const dbChar = process.env.DB_CHAR;
 
     const app = express();
     app.use(express.json());
 
     const port = process.env.PORT || 3000;
-    const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     const options = {
         useUnifiedTopology: true
     };
 
     const client = await mongodb.MongoClient.connect(connectionString, options);
-    const db = client.db("personagem_db");
-    const personagens = db.collection("personagem");
+    const db = client.db("personagens_db");
+    const personagens = db.collection("personagens");
 
     const getPersonagensValidos = () => personagens.find({}).toArray();
     const getPersonagemById = async(id) => personagens.findOne({_id: ObjectId(id)});
