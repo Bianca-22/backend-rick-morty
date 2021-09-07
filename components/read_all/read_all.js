328ecd0eaa485636c30d27621678d555498692d4
violigon <vinicius@oligon.com.br>
2021-09-06 22:57:08 -0300

    fix(routes-read-all )

--- components/read_all/read_all.js
+++ components/read_all/read_all.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const mongodb = require("mongodb");
-require("dotenv").config();
-
 (async () => {
-    const dbUser = process.env.DB_USER;
-    const dbPassword = process.env.DB_PASSWORD;
-    const dbName = process.env.DB_NAME;
-    const dbChar = process.env.DB_CHAR;
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbName = process.env.DB_NAME;
+  const dbChar = process.env.DB_CHAR;
+  const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+  const options = {
+    useUnifiedTopology: true,
+  };
+  const client = await mongodb.MongoClient.connect(connectionString, options);
 
-    const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-    const options = { useUnifiedTopology: true };
+  const db = client.db("blue_db");
+  const personagens = db.collection("personagens");
 
-    const client = await mongodb.MongoClient.connect(connectionString, options);
-    const db = client.db("personagens_db");
-    const personagens = db.collection("personagens");
+  const getPersonagensValidas = () => personagens.find({}).toArray();
 
-    const getPersonagensValidos = () => personagens.find({}).toArray();
+  //Middleware - especifica que é esse o import do router no index que queremos utilizar
+  router.use(function timelog(req, res, next) {
+    next();
+    console.log("Time: ", Date.now());
+  });
 
-    router.use(function timelog(req, res, next) {
-        next();
-    });
 
-    router.get('/personagens', async (req,res) => {
-        res.send(await getPersonagensValidos())
-    });
+//[GET] GetAllPersonagens
+
+router.get("/", async (req, res) => {
+  res.send(await getPersonagensValidas());
 });
+})();
+  
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,52 +1,94 @@
 const express = require("express");
-require("express-async-error");
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
 require("dotenv").config();
+require("express-async-errors");
 var cors = require("cors");
-
+//requires de endpoints
 const home = require("./components/home/home");
 const readAll = require("./components/read_all/read_all");
-const create = require("./components/create/create");
-const update = require("./components/update/update");
-const deletar = require("./components/delete/delete");
 const readById = require("./components/read_by_id/read_by_id");
+const del = require("./components/delete/delete");
+const atualizar = require("./components/update/update");
+const criar = require("./components/create/create");
 
 (async () => {
-    const app = express();
-    app.use(express.json());
+	const dbUser = process.env.DB_USER;
+	const dbPassword = process.env.DB_PASSWORD;
+	const dbName = process.env.DB_NAME;
+	const dbChar = process.env.DB_CHAR;
+
+	const app = express();
+	app.use(express.json());
+
 	const port = process.env.PORT || 3000;
+	const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-	// CORS - Novo
+	const options = {
+		useUnifiedTopology: true,
+	};
+
+	console.info("Conectando ao MongoDB Atlas...");
+
+	const client = await mongodb.MongoClient.connect(connectionString, options);
+
+	console.info("Conexão estabelecida com o MongoDB Atlas!");
+
+	const db = client.db("blue_db");
+	const personagens = db.collection("personagens");
+
+	const getPersonagensValidas = () => personagens.find({}).toArray();
+
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
+
+	//CORS
+
+	/* app.all("/*", (req, res, next) => {
+		res.header(
+			"Access-Control-Allow-Origin",
+			"https://front-rick-morty-blue.herokuapp.com"
+		);
+
+		res.header("Access-Control-Allow-Methods", "*");
+
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+		);
+
+		next();
+	}); */
+
 	app.use(cors());
-
 	app.options("*", cors());
 
-	// CORS - Antigo
-		// app.all("/*", (req, res, next) => {
-		// 	res.header("Access-Control-Allow-Origin", "*");
-		// 	res.header("Access-Control-Allow-Methods", "*");
-		// 	res.header(
-		// 		"Access-Control-Allow-Headers",
-		// 		"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-		// 	);
-		// 	next();
-		// });
+	//Criando a rota home
+	app.use("/home", home);
 
-	app.use('/home', home);
+	// criando a rota read-all
+	app.use("/personagens/read-all", readAll);
 
-    app.use('/read_all', readAll);
+	//[GET] getPersonagemById
 
-    app.use('/read_by_id', readById);
+	app.use("/personagens/read-by-id/", readById);
 
-    app.use('/create', create);
+	//[POST] Adicona personagem
+	app.use("/personagens/create/", criar);
 
-    app.use('/update', update);
+	//[PUT] Atualizar personagem
+	app.use("/personagens/update/", atualizar);
 
-	app.use('/delete', deletar);
+	//[DELETE] Deleta um personagem
+	app.use("/personagens/delete/", del);
 
+	//Tratamento de erros
+	//Middleware verificar endpoints
 	app.all("*", function (req, res) {
 		res.status(404).send({ message: "Endpoint was not found" });
 	});
 
+	//Middleware -> Tratamento de erro
 	app.use((error, req, res, next) => {
 		res.status(error.status || 500).send({
 			error: {
@@ -56,7 +98,7 @@ const readById = require("./components/read_by_id/read_by_id");
 		});
 	});
 
-    app.listen(port, ()=>{
-    console.info(`App rodando em http://localhost:${port}/home`)
-    });
+	app.listen(port, () => {
+		console.info(`App rodando em http://localhost:${port}/home`);
+	});
 })();
